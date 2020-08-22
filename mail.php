428042afa88fdecd1254e57339dbf9380dc746d5
maks95x <70012274+maks95x@users.noreply.github.com>
2020-08-22 15:49:08 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -28,7 +28,7 @@ $mail->addAddress('samokish.m.a@gmail.com');     // Кому будет уход
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
+$mail->Subject = 'Заявка с сайта';
 $mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone .;
 $mail->AltBody = '';
 
